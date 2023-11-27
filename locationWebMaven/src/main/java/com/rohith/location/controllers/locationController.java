2eb1f44ac3ff76c2642903865093037e0c0b29a4
rohithreddybc <rohithreddybc98@gmail.com>
2023-11-26 19:20:15 -0500

    Mapping jsps

--- locationWebMaven/src/main/java/com/rohith/location/controllers/locationController.java
+++ locationWebMaven/src/main/java/com/rohith/location/controllers/locationController.java
@@ -1,12 +1,17 @@
 package com.rohith.location.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class locationController {
 	
-	@RequestMapping("/showCreate")
+	@GetMapping("/")
+    public String index() {
+        return "index";
+    }
+	@GetMapping("/showCreate")
 	public String showCreate() {
 		return "createLocation"; //jsp file name
 	}
