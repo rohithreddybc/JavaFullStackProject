189838b2bf3d80fe744cdf1638dcb01b20034e35
rohithreddybc <rohithreddybc98@gmail.com>
2023-12-03 00:43:49 -0500

    changes with server and jsp

--- locationWebMaven/src/main/java/com/rohith/location/LocationWebApplication.java
+++ locationWebMaven/src/main/java/com/rohith/location/LocationWebApplication.java
@@ -2,8 +2,10 @@ package com.rohith.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.rohith.location.controllers,"+"com.rohith.location.services,"+"com.rohith.location.entities")
 public class LocationWebApplication {
 
 	public static void main(String[] args) {
